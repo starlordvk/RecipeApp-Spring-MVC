2f26d2aeec576d1d1881e831e5734a4265a4dc71
starlordvk <chocoshake2007@gmail.com>
2018-03-19 19:14:58 +0530

    Add spring data repositories

---
+++ src/main/java/com/example/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.recipe.repositories;
+
+import com.example.recipe.model.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
