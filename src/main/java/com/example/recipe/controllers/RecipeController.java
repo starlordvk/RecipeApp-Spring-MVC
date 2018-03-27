fa8c926102b62bb36e58c8fb31214f7e09484e9a
starlordvk <chocoshake2007@gmail.com>
2018-03-27 22:09:28 +0530

    Add recipe form and recipe controller methods to create a new recipe

--- src/main/java/com/example/recipe/controllers/RecipeController.java
+++ src/main/java/com/example/recipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.commands.RecipeCommand;
 import com.example.recipe.model.Recipe;
 import com.example.recipe.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +22,20 @@ public class RecipeController {
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipes(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeForm";
+
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand=recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 
 }
